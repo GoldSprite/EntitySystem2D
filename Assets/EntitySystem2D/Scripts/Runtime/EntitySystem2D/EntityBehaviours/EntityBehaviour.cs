1fd86331f34c8b48dd4d6f7a1cf476da4d4949d8
shikeik <30600841+shikeik@users.noreply.github.com>
2024-04-02 03:32:50 +0800

    x

--- Assets/EntitySystem2D/Scripts/Runtime/EntitySystem2D/EntityBehaviours/EntityBehaviour.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace GoldSprite.UnityPlugins.EntitySystem2D {
-    public abstract class EntityBehaviour {
-        public abstract void Init(EntityBehaviourConstructor ent, int priority);
-    }
-}
--- Packages/com.unity.upm.develop/README.md
+++
@@ -1,3 +0,0 @@
-# Package Development
-
-The purpose of this package is to add development flow and some functionalities to the package manager UI.
